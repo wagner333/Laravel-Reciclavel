09f49ea809b9ef7cdc663f3d877983478eeddc5f
wagner <wwfome6@gmail.com>
2024-08-25 22:09:55 -0300

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model as Eloquent;
+
+class User extends Eloquent
+{
+    use \Illuminate\Notifications\Notifiable;
+    use \Laravel\Sanctum\HasApiTokens;
+    use \Illuminate\Database\Eloquent\Factories\HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
